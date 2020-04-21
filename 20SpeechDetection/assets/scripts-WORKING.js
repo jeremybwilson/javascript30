b8d89aacd4da934543b41d53022db4e552856a3f
Jeremy Wilson <jeremy_b_wilson@hotmail.com>
2020-04-21 16:38:52 -0700

    Reduce method and speech detection exercises
    - reworked files into new subfolders, .css and .js files

--- 20SpeechDetection/assets/scripts-WORKING.js
+++ 20SpeechDetection/assets/scripts-WORKING.js
@@ -8,12 +8,29 @@ const words = document.querySelector('.words');
 words.appendChild(p);
 
 recognition.addEventListener('result', event => {
-  console.log(event.results);
+  // console.log(event.results);
   const transcript = Array.from(event.results)
-  .map(result => result[0])
-  .map(result => result.transcript)
-  .join('');
+    .map(result => result[0])
+    .map(result => result.transcript)
+    .join('');
   // console.log(transcript);
+
+  // censored words
+  const poopScript = transcript.replace(/poop|poo|fuck|frack|damn|pussy|cock|shit|dump/gi, '💩');
+  p.textContent = poopScript;
+
+  if (event.results[0].isFinal){
+    p = document.createElement('p');
+    words.appendChild(p);
+  }
+
+  if (transcript.includes('unicorn')){
+    console.log('🦄🦄🦄🦄🦄🦄');
+  }
+
 })
+
+// adding an event listener for the 'end' event
+recognition.addEventListener('end', recognition.start);
 
 recognition.start(event);
