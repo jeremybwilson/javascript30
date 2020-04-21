76df7639112e47bf6fc2774aa21c71aeb498e1b5
Jeremy Wilson <jeremy_b_wilson@hotmail.com>
2020-04-21 11:23:16 -0700

    LocalStorage exercises
    - reworked files into new subfolders, .css and .js files

--- 15LocalStorage/assets/localstorage-WORKING-2.js
+++ 15LocalStorage/assets/localstorage-WORKING-2.js
@@ -8,63 +8,73 @@ const items = JSON.parse(localStorage.getItem('items')) || [];  // if no values 
 
 // create a function to handle the addItem event
 function addItem(event){
-
   // stop the form from submitting with event.preventDefault();
   event.preventDefault();
-  // create a variable to store the text from the form field 'item name'
+  // take the text of the submitted form field and add it to the list
+  // capture text from form field
   const text = (this.querySelector('[name=item]')).value;
-  // create a item object
+  // create an item object
   const item = {
-    // text: text,  // instead us the ES6 shorthand property
-    text,
+    text: text,
     done: false
-  };
+  }
   // console.log(item);
+  // push to the items array
   items.push(item);
   populateList(items, itemsList);
-  // set our items array above, 'items' into local storage
   localStorage.setItem('items', JSON.stringify(items));
+  // now reset the form element
   this.reset();
+  // console.log(`.:: DEBUG ::. value of this: `, this);
 }
 
-// create a function to populate the list
-/// the populateList function needs two things: 1) the empty 'plates' object, and 2) and a place to put the html called 'platesList'
-function populateList(plates = [], platesList){  // pre define an empty plates array
-  console.log('Populating the list');
-  // loop over every single item in our array, and
-  // stick the plates.map() results directly into the returned HTML
-  platesList.innerHTML = plates.map((plate, i) => {  // map will take in an array of raw data and return an array of some other raw data
-    // return some HTML
+function populateList(plates = [], platesList){
+  // console.log(`.:: DEBUG ::. populateList function here.`);
+  platesList.innerHTML = plates.map((plate, i) => {
     return `
       <li>
-        <input type="checkbox" data-index=${i} id="item${i}" ${ plate.done ? 'checked' : '' } />
+      <input type="checkbox" data-index=${i} id="item${i}" ${ plate.done !== false ? 'checked' : ''} />
         <label for="item${i}">${plate.text}</label>
       </li>
     `;
   }).join('');
-
 }
 
 function toggleDone(event){
-  // event delegation -> responsible parent, irresponsible child
+  // event delegation -> very responsible parent, irresponsible child
   // want to check if the target matches the thing that we are looking for
-  if(!event.target.matches('input')) return;  // skip this unless it's an input
+  if (!event.target.matches('input')) return;  // skip this unless it's an input
+  console.log(event.target);
+
   // get the element
   const el = event.target;
+  // console.log(el.dataset.index);  // this is why we put a data-index on each of the checkboxes
+
   // get the element index
   const index = el.dataset.index;
-  console.log(el.dataset.index);  // this is why we put a data-index on each of the checkboxes
+
   // take our items from the index, set it to itself but opposite
-  items[index].done = !items[index].done;
+  items[index].done =!items[index].done;
+
   // set the items to localStorage
   localStorage.setItem('items', JSON.stringify(items));
+
   // run populateList function once more
   populateList(items, itemsList);
+
 }
+// function to add taco when checkbox is checked
+// const addTaco = (event) => {
+//   // prevent the default page refresh action
+//   event.preventDefault();
+//   console.log(`.:: DEBUG ::. List item (input) was checked`);
+// }
 
 // create eventListeners here
-// cover all of our bases and listen to the submit event
+
+// add an event listener for when submit button is clicked
 addItems.addEventListener('submit', addItem);
+// add an event listener for when checkbox is checked
 itemsList.addEventListener('click', toggleDone);
 
 // run populate list once more, remember that items doesn't exist on page load unless we read it in from localStorage first, see line #7
