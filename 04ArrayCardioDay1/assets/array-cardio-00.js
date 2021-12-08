548e05b3e9e5849156fcda90e1bc0397a4dd9cc6
Jeremy Wilson <jeremy_b_wilson@hotmail.com>
2021-12-07 16:15:05 -0800

    Updated Day 1 cardio exercise files

--- 04ArrayCardioDay1/assets/array-cardio-00.js
+++ 04ArrayCardioDay1/assets/array-cardio-00.js
@@ -1,5 +1,5 @@
-  // Get your shorts on - this is an array workout!
-  // ## Array Cardio Day 1
+// Get your shorts on - this is an array workout!
+// ## Array Cardio Day 1
 
 // Some data we can work with
 import { people }  from './people.js';
---
+++ 04ArrayCardioDay1/assets/data.js
@@ -0,0 +1,7 @@
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+
+// this is a named export
+export { data };
+
+// this is a default export
+// export default data;
